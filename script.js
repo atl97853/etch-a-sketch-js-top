9a6f14d069657aa09948d2624cc81c0f16901ccc
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-05-19 22:36:22 -0600

    Test functionality to set grid using a popup asking for the number of squares

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+// Functions
+
+// Variables
 const divMain = document.querySelector('.main-container');
-
-// array of divs 
-
+ let size = Number(prompt('Create your grid'));
+ let totalPixels = size * 20;
+// Create array with divs 
 const divsArray = [];
-
-for (let i = 0; i < 32 * 32; i++) {
+for (let i = 0; i < size * size; i++) {
     let divNew = document.createElement('div');
     divNew.classList.add('div-new');
     divsArray.push(divNew);
-}
-
+};
+// Testing array with divs 
 console.log(divsArray.length);
 console.log(divsArray);
+// Append divs in the DOM
+divsArray.forEach(div => divMain.appendChild(div)); 
 
-divsArray.forEach(div => divMain.appendChild(div));
+// set grid 
 
+divMain.style.maxWidth = `${totalPixels}px`;
+divMain.style.maxHeight = `${totalPixels}px`;
