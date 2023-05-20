77d5f490ba39b5042e0383fc7afe983eada46040
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-05-19 23:58:04 -0600

    Separate code for sections and make it more readable

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-// Functions
-
 // Variables
 const divMain = document.querySelector('.main-container');
- let size = Number(prompt('Create your grid'));
- let totalPixels = size * 20;
-// Create array with divs 
 const divsArray = [];
+//  let size = Number(prompt('Create your grid'));
+let size = 32;
+let totalPixels = size * 20;
+
+// Create array with divs 
 for (let i = 0; i < size * size; i++) {
     let divNew = document.createElement('div');
     divNew.classList.add('div-new');
     divsArray.push(divNew);
 };
+
 // Testing array with divs 
 console.log(divsArray.length);
 console.log(divsArray);
-// Append divs in the DOM
+
+// Append divs in the DOM document
 divsArray.forEach(div => divMain.appendChild(div)); 
 
-// set grid 
-
+// Set grid 
 divMain.style.maxWidth = `${totalPixels}px`;
 divMain.style.maxHeight = `${totalPixels}px`;
+
+// Add and change divs color
