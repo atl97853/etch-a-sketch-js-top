81c5ce4bb13a57cf351a901c2445496259dc74b1
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-05-21 23:59:53 -0600

    Start again

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
+// // Set range input 
+// const inputRange = document.getElementById('my-slider');
+// const divRange = document.getElementById('slider-value');
+
+
+// const idk = inputRange.addEventListener('click', () => {
+//     const number = Number(inputRange.value);
+//     return number;
+// });
+
 // Variables
 const divMain = document.querySelector('.main-container');
 const divsArray = [];
-let divsTotal = 30;
+
+// let divsTotal = Number(prompt('Max: 32 pixel'));
+let divsTotal = 16;
 let pixelsTotal = 640/divsTotal;
 
 // Create array with divs 
 for (let i = 0; i < divsTotal * divsTotal; i++) {
     let divNew = document.createElement('div');
     divNew.classList.add('div-new');
+    // Set div size 
     divNew.style.width = `${pixelsTotal}px`;
     divNew.style.height = `${pixelsTotal}px`;
+
     divsArray.push(divNew);
 };
 
@@ -20,11 +34,8 @@ console.log(divsArray);
 // Append divs in the DOM document
 divsArray.forEach(div => divMain.appendChild(div)); 
 
-// // Set grid size width and height 
-// divMain.style.maxWidth = `${totalPixels}px`;
-// divMain.style.maxHeight = `${totalPixels}px`;
-
 // Add and change divs color
 divsArray.forEach(div => div.addEventListener('click', (div) => {
     div.target.style.background = 'blue'; 
 }));
+
