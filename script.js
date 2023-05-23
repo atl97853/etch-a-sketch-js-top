cef0d110d76b31c30642a3eb3a9953811ebd90a2
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-05-23 00:34:21 -0600

    Update

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
-// // Set range input 
-// const inputRange = document.getElementById('my-slider');
-// const divRange = document.getElementById('slider-value');
+    // Variables
+    const divMain = document.querySelector('.grid-container');
+    const divsArray = [];
 
+    // let divsTotal = Number(prompt('Max: 32 pixel'));
+    let divsTotal = 32;
+    let pixelsTotal = 640/divsTotal;
 
-// const idk = inputRange.addEventListener('click', () => {
-//     const number = Number(inputRange.value);
-//     return number;
-// });
+    // Create array with divs 
+    for (let i = 0; i < divsTotal * divsTotal; i++) {
+        let divNew = document.createElement('div');
+        divNew.classList.add('div-new');
+        // Set div size 
+        divNew.style.width = `${pixelsTotal}px`;
+        divNew.style.height = `${pixelsTotal}px`;
 
-// Variables
-const divMain = document.querySelector('.main-container');
-const divsArray = [];
+        divsArray.push(divNew);
+    };
 
-// let divsTotal = Number(prompt('Max: 32 pixel'));
-let divsTotal = 16;
-let pixelsTotal = 640/divsTotal;
+    // Testing array with divs 
+    console.log(divsArray.length);
+    console.log(divsArray);
 
-// Create array with divs 
-for (let i = 0; i < divsTotal * divsTotal; i++) {
-    let divNew = document.createElement('div');
-    divNew.classList.add('div-new');
-    // Set div size 
-    divNew.style.width = `${pixelsTotal}px`;
-    divNew.style.height = `${pixelsTotal}px`;
+    // Append divs in the DOM document
+    divsArray.forEach(div => divMain.appendChild(div)); 
 
-    divsArray.push(divNew);
-};
-
-// Testing array with divs 
-console.log(divsArray.length);
-console.log(divsArray);
-
-// Append divs in the DOM document
-divsArray.forEach(div => divMain.appendChild(div)); 
-
-// Add and change divs color
-divsArray.forEach(div => div.addEventListener('click', (div) => {
-    div.target.style.background = 'blue'; 
+    // Add and change divs color
+    divsArray.forEach(div => div.addEventListener('click', (div) => {
+        div.target.style.background = '#1E2022'; 
 }));
 
